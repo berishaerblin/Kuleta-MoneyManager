1ff6007fc27fa04a2cac693da178c5a3698e6392
ArtanXhaha <artanxhaha@gmail.com>
2017-01-17 20:29:07 +0100

    Shfaqja e te gjitha kategorive ne Aktivitetin e Kategorise.

--- app/src/main/java/com/berishaerblin/moneymanager/Category/Kategoria.java
+++ app/src/main/java/com/berishaerblin/moneymanager/Category/Kategoria.java
@@ -2,12 +2,24 @@ package com.berishaerblin.moneymanager.Category;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.berishaerblin.moneymanager.Category.Pasqyra.CustomAdapterCategory;
+import com.berishaerblin.moneymanager.Category.Pasqyra.CustomAdapterPasqyra;
 import com.berishaerblin.moneymanager.R;
+import com.berishaerblin.moneymanager.dataBase.DataBaseSource;
+import com.berishaerblin.moneymanager.dataBase.model.Category;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -15,17 +27,34 @@ import com.berishaerblin.moneymanager.R;
  */
 public class Kategoria extends Fragment {
 
+    DataBaseSource db;
+    CustomAdapterCategory customAdapterCategory;
+    List<Category> lista;
+    ListView listView;
 
-    public Kategoria() {
-        // Required empty public constructor
+    public Kategoria() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        db = new DataBaseSource(getContext());
+        lista = new ArrayList<Category>();
+        lista.addAll(db.getAllCategories());
+        customAdapterCategory = new CustomAdapterCategory(getContext(),lista);
+
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kategoria, container, false);
+        View v = inflater.inflate(R.layout.fragment_kategoria, container, false);
+        listView = (ListView)v.findViewById(R.id.lstKategoria);
+        listView.setAdapter(customAdapterCategory);
+
+        return v;
+
+
     }
 
 }
